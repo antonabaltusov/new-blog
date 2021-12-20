9ac853023adffffa8c23f2e8a97d3ccd332c8b96
anton <sims0204@gmail.com>
2021-12-20 17:57:02 +0400

    hw1

---
+++ nestjs-new-blog/src/news/news.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { News, NewsService } from './news.service';
+
+@Controller('news')
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Get('all')
+  getAll(): News[] {
+    return this.newsService.getAll();
+  }
+
+  @Get('/:id')
+  get(@Param('id') id: string): News {
+    const idInt = parseInt(id);
+    return this.newsService.find(idInt);
+  }
+
+  @Post()
+  create(@Body() news: News): News {
+    return this.newsService.create(news);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string): string {
+    const idInt = parseInt(id);
+    const isRemove = this.newsService.remove(idInt);
+    return isRemove ? 'Новость удалена' : 'Передан неверный идентификатор';
+  }
+
+  @Patch()
+  change(@Body() news: News): string {
+    const isChange = this.newsService.change(news);
+    return isChange ? 'Новость изменена' : 'Передан неверный идентификатор';
+  }
+}
